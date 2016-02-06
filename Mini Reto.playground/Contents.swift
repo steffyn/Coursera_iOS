64249b1e29a9edb9072b104cf81631d7c5b34495
steffyn <katherinehernandez@Katherines-MacBook-Air.local>
2016-02-06 17:03:03 -0600

    Version final

--- Mini Reto.playground/Contents.swift
+++ Mini Reto.playground/Contents.swift
@@ -1,14 +1,17 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-//Doy por entendido que el cero matematicamente es multiplo de 5
+//Doy por entendido que tengo que evaluar cada número, no importando si esté cumple más de dos condiciones
 for i in 0...100{
-    if i%5 == 0{
+    
+    if i%5 == 0 {
         print("\(i) BINGO!!")
-    }else if i%2 == 0{
+    }
+    if i%2 == 0{
         print("\(i) PAR!!")
     }else{
         print("\(i) IMPAR!!")
+    }
+    if i>=30 && i<=40{
+        print("\(i) Viva Swift!!")
     }
 }
