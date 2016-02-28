8befe9f5afc642c7623d27fde509138b08f94204
steffyn <katherinehernandez@katherines-macbook-air.local>
2016-02-27 20:36:27 -0600

    Initial Commit

---
+++ MasFeliz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasFeliz
+//
+//  Created by Katherine Hernandez on 27/2/16.
+//  Copyright © 2016 Katherine Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
